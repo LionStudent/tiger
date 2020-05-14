9a2ae4e02196ed143408ab8ac74f05dbcc1c0ce3
tigerstudent <tiger.student@yandex.com>
2020-05-14 22:20:47 +0000

    Made script.py more user friendly

--- script.py
+++ script.py
@@ -3,12 +3,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 import math
 
-maxRange = 6.28
 
-xAxis = sine.sinePlotX(maxRange, 2)
+#Change this code:
+
+#The maximum X-value on your chart
+maxRange = 6.28
+#The number of digits on the graph
+digits = 2
+#To learn how to change the style of the graph, go to https://matplotlib.org/2.1.1/api/_as_gen/matplotlib.pyplot.plot.html
+style = 'g-'
+
+
+#Leave this code alone:
+xAxis = sine.sinePlotX(maxRange, digits)
 yAxis = sine.sinePlotY(xAxis)
 
-style = 'ro'
 plt.plot(xAxis , yAxis , style)
 plt.axis([0, maxRange + 0.1, -1, 1])
 filename = 'graph.png'
